6a9eb9fda0f9945db17fe2a04e808f8c21e1d3e1
ibatalkin <iurii.batalkin@gmail.com>
2021-12-07 18:39:07 +0100

    update replace and restart with valid link

--- replace_and_restart_snarkos.sh
+++ replace_and_restart_snarkos.sh
@@ -3,9 +3,8 @@
 systemctl stop aleod
 
 cd $HOME
-wget https://github.com/ibatalkin/aleo_update_scripts/blob/f8f549c8e1dc2e8cb5e1ef3bc89d424910ba8f90/binary/snarkos
-chmod +x snarkos
-mv snarkos /usr/bin/snarkos
+wget https://github.com/ibatalkin/aleo_update_scripts/raw/main/binary/snarkos -O /usr/bin/snarkos
+chmod +x /usr/bin/snarkos
 
 systemctl restart aleod
 
