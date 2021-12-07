f8f549c8e1dc2e8cb5e1ef3bc89d424910ba8f90
ibatalkin <iurii.batalkin@gmail.com>
2021-12-07 01:31:57 +0100

    fixed bugs in updater script

--- update_ironfish.sh
+++ update_ironfish.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 snarkos_folder=~/snarkOS
+upd_folder=~/aleo_update_scripts
 
 if [ -d $snarkos_folder ]; then
 	cd $snarkos_folder
 	git pull origin testnet2
 	cargo install --path .
-	cp $snarkos_floder/target/release/snarkos &0/binary/snarkos
+	cp target/release/snarkos $upd_folder/binary/snarkos
 	
-	cd &0
-	git add snarkos
+	cd $upd_folder
+	git add binary/
+	git commit -m "update snarkos binary"
 	git push
 else
 	echo "Can't find snarkOS flolder"
