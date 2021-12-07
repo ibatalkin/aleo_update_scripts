82d5b544bebab1c46da42a3adb6029cf7a249b94
ibatalkin <iurii.batalkin@gmail.com>
2021-12-07 02:14:09 +0100

    update script

--- replace_and_restart_snarkos.sh
+++ replace_and_restart_snarkos.sh
@@ -2,7 +2,7 @@
 
 systemctl stop aleod
 
-wget https://github.com/ibatalkin/aleo_update_scripts/blob/f8f549c8e1dc2e8cb5e1ef3bc89d424910ba8f90/binary/snarkos -P /usr/bin/
+wget https://github.com/ibatalkin/aleo_update_scripts/blob/f8f549c8e1dc2e8cb5e1ef3bc89d424910ba8f90/binary/snarkos -O /usr/bin/snarkos
 chmod +x /usr/bin/snarkos
 
 systemctl restart aleod
