196f950c81d6e921dfc2942f31f5b004a4b36d99
ibatalkin <iurii.batalkin@gmail.com>
2021-12-07 01:53:29 +0100

    fix and enable replace and start snarkos

--- replace_and_restart_snarkos.sh
+++ replace_and_restart_snarkos.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#systemctl stop aleod
+systemctl stop aleod
 
-#wget https://github.com/ibatalkin/aleo_update_scripts/blob/f8f549c8e1dc2e8cb5e1ef3bc89d424910ba8f90/binary/snarkos -O /bin/snarkos
+wget https://github.com/ibatalkin/aleo_update_scripts/blob/f8f549c8e1dc2e8cb5e1ef3bc89d424910ba8f90/binary/snarkos -O /bin/snarkos
+chmod +x /bin/snarkos
 
-#systemctl restart aleod
+systemctl restart aleod
 
-wget https://github.com/ibatalkin/aleo_update_scripts/blob/f8f549c8e1dc2e8cb5e1ef3bc89d424910ba8f90/binary/snarkos -O $HOME/snarkos_test_binary
+
